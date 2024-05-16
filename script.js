b42df1da0e05969f89b8aacb97b1a811ce56f656
Shruthi-10 <150574359+Shruthi-10@users.noreply.github.com>
2024-05-16 06:33:36 -0700

    Third commit

--- script.js
+++ script.js
@@ -1,32 +1,47 @@
 const topicElement = document.querySelector(".topic");
 const countElement = document.querySelector(".count");
+const body = document.querySelector("body");
+const success = document.querySelector(".success");
+const dance = document.querySelector(".dance");
 
-const learning = {};
-learning.topic = "JS";
-learning.learningGoals = ["Build programs", "Learn Interaction", "something", "more something"];
-learning.category = "Front End Development";
-learning.topicImportance = "High";
+const learning = {
+    topic: "JS",
+    learningGoals: [
+        "Understand programming fundamentals",
+        "Have Fun",
+        "Build JS programs"
+    ],
+    category: "Front End Development",
+    topicImportance: "high",
+    hoursThisWeek: 2,
+    weeklyHourGoal: 7,
+    achievedStudyGoal: false,
+    addStudyTime: function (hours) {
+        this.hoursThisWeek += hours;
+        if (this.hoursThisWeek >= this.weeklyHourGoal) {
+            this.achievedStudyGoal = true;
+            this.celebrate();
+        }
+    },
+    celebrate: function() {
+        body.classList.add("celebrate");
+        success.classList.remove("hide");
+        dance.classList.remove("hide");
+    }
+};
 
-console.log(learning);
+topicElement.innerText = `✔️ I'm learning ${learning.topic}.`;
+countElement.innerText = `✔️ I have ${learning.learningGoals.length} learning goals.`;
 
-learning.topic = "javaScript";
-console.log(learning);
-
-learning.learningGoals.splice(1,1);
-
-
-topicElement.innerText = `I'm learning ${learning.topic}.`;
 topicElement.classList.remove("hide");
-console.log(topicElement);
+countElement.classList.remove("hide");10
 
-countElement.innerText = `I have ${learning.learningGoals.length} learning goals`;
-countElement.classList.remove("hide");
+learning.addStudyTime(10);
 
+/* 
+Call Methods
 
-/*
-Set the innerText of the countElement to the string “I have <number> learning goals.” Substitute <number> with the length of the learningGoals property array.
-Remove the class “hide” that’s given to countElement. 
-Reload the browser window to see the changes.
-When finished, submit a link to your sandbox below!
-
+Inside the addStudyTime method’s if statement, call the celebrate() method. Hint: Don’t forget the this keyword!
+At the bottom of the script.js file, call learning.addStudyTime(). As an argument, pass the method a number that will make the value of hoursThisWeek greater than the weeklyHourGoal. This will trigger celebrate() to run.
+When complete, submit your link below and celebrate your hard work! 🎉🥳
 */
